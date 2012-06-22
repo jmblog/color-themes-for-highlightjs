cec9900db19bea5a8e56a63a8ba9fe2e23baaf45
Yoshihide Jimbo <yjimbo@gmail.com>
2012-06-22 23:13:15 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+color-themes-for-highlightjs
+=====================================
+
+Color themes for highlight.js.
+
+see http://jmblog.github.com/color-themes-for-highlightjs/
---
+++ build/screenshot.sh
@@ -0,0 +1,6 @@
+#/usr/bin/sh
+webkit2png --scale=1 --clipped --clipwidth=288 --clipheight=183 --dir=./intermediate/img --filename=tomorrow-night http://highlightjs.local:8000/tomorrow-night/preview.html
+webkit2png --scale=1 --clipped --clipwidth=288 --clipheight=183 --dir=./intermediate/img --filename=tomorrow http://highlightjs.local:8000/tomorrow/preview.html
+webkit2png --scale=1 --clipped --clipwidth=288 --clipheight=183 --dir=./intermediate/img --filename=tomorrow-night-eighties http://highlightjs.local:8000/tomorrow-night-eighties/preview.html
+webkit2png --scale=1 --clipped --clipwidth=288 --clipheight=183 --dir=./intermediate/img --filename=tomorrow-night-blue http://highlightjs.local:8000/tomorrow-night-blue/preview.html
+webkit2png --scale=1 --clipped --clipwidth=288 --clipheight=183 --dir=./intermediate/img --filename=tomorrow-night-bright http://highlightjs.local:8000/tomorrow-night-bright/preview.html
