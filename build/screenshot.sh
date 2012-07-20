61f6934198580468d9925359bfca5d972a3b310d
Yoshihide Jimbo <yjimbo@gmail.com>
2012-07-20 17:04:20 +0900

    add hemisu themes

--- build/screenshot.sh
+++ build/screenshot.sh
@@ -4,3 +4,5 @@ webkit2png --scale=1 --clipped --clipwidth=288 --clipheight=183 --dir=./intermed
 webkit2png --scale=1 --clipped --clipwidth=288 --clipheight=183 --dir=./intermediate/img --filename=tomorrow-night-eighties http://highlightjs.local:8000/tomorrow-night-eighties/preview.html
 webkit2png --scale=1 --clipped --clipwidth=288 --clipheight=183 --dir=./intermediate/img --filename=tomorrow-night-blue http://highlightjs.local:8000/tomorrow-night-blue/preview.html
 webkit2png --scale=1 --clipped --clipwidth=288 --clipheight=183 --dir=./intermediate/img --filename=tomorrow-night-bright http://highlightjs.local:8000/tomorrow-night-bright/preview.html
+webkit2png --scale=1 --clipped --clipwidth=288 --clipheight=183 --dir=./intermediate/img --filename=hemisu-dark http://highlightjs.local:8000/hemisu-dark/preview.html
+webkit2png --scale=1 --clipped --clipwidth=288 --clipheight=183 --dir=./intermediate/img --filename=hemisu-light http://highlightjs.local:8000/hemisu-light/preview.html
